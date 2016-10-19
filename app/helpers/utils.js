e8e92751065c1c5c052d43f1d9d1d7b50762db68
Chris Praktish <chris.praktish@gmail.com>
2016-10-19 19:39:09 -0400

    Step 8: Finish the UI for /forecast
    * At this point the component that becomes active when a user navigates to /forecast should have all the data it needs to render properly. So, make that UI look nice.

---
+++ app/helpers/utils.js
@@ -0,0 +1,40 @@
+var daysMap = {
+  '0':"Sunday",
+  '1':"Monday",
+  '2':"Tuesday",
+  '3':"Wednesday",
+  '4':"Thursday",
+  '5':"Friday",
+  '6':"Saturday"
+};
+
+var monthsMap = {
+  '0':"Jan",
+  '1':"Feb",
+  '2':"Mar",
+  '3':"Apr",
+  '4':"May",
+  '5':"Jun",
+  '6':"Jul",
+  '7':"Aug",
+  '8':"Sep",
+  '9':"Oct",
+  '10':"Nov",
+  '11':"Dec"
+};
+
+function convertTemp (kelvin) {
+  return (kelvin - 273.15)* 1.8000 + 32
+};
+
+function getDate(unixTimestamp) {
+  var date = new Date(unixTimestamp * 1000);
+  var day = daysMap[date.getDay()]
+  var month = monthsMap[date.getMonth()] + ' ' + date.getDate();
+  return day + ', ' + month;
+}
+
+module.exports = {
+  convertTemp: convertTemp,
+  getDate: getDate
+}
